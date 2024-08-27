b044f806efac052ce6cf25afa57229b827c295f0
Andrea Gulli <andreagulli11@gmail.com>
2024-08-27 16:54:52 +0200

    TICKET

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Prelevo gli elementi dal DOM 
 const cellRnd = document.querySelectorAll('.casual-numbers')
-// Funzione di avvio
+// Funzione di avvio 
 function Start() {
     let num = new Array(5)
     for (let i = 0; i < num.length; i++) {
@@ -8,7 +8,25 @@ function Start() {
         console.log(num[i])
         cellRnd[i].innerHTML = num[i]
     }
-}
+    // Faccio partire un timer di 30 secondi e azzero e celle
+    setTimeout(() => {
+        for (let i = 0; i < num.length; i++) {
+            cellRnd[i].innerHTML = '?'
+        }
+       
+    // Chiedo all'utente di indovinare i cinque numeri 
+        let counter = 0 
+
+        for (let i = 0; i < num.length; i++) {
+            let userInput = parseInt(prompt('Inserisci i cinque numeri che hai visto! numero' + (i + 1) ))
+        // Controllo quali e quanti numeri l'utente ha indovinato
+            if (userInput == num[i]) {
+                counter++
+            }
+        }
+        console.log('Complimenti! hai indovinato' + counter + 'numeri !')
+    }, 3000);}
+
 
 // Funzione che genera numeri random
 function Rnd() {
