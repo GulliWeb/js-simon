0c193daecdccfc585cfab70c360dcb64c6b929ca
Andrea Gulli <andreagulli11@gmail.com>
2024-08-27 18:14:36 +0200

    fixed win logic

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const userInput = document.querySelectorAll('.user-input')
 let num = new Array(5)
 // Funzione di avvio 
 function Start() {
+    // Azzero le celle
+    userInput.forEach(input => input.value = "");
     for (let i = 0; i < num.length; i++) {
         num[i] = Rnd()
         console.log(num[i])
@@ -28,9 +30,14 @@ function guessRnd() {
       let counter = 0 
       let numRnd = num
 
-      for (let i = 0; i < num.length; i++) {
-      // Controllo quali e quanti numeri l'utente ha indovinato
-          if (userInput[i].value == numRnd[i]) counter++
+      for (let i = 0; i < userInput.length; i++) {
+        const userValue = parseInt(userInput[i].value);
+        const index = numRnd.indexOf(userValue);
+        if (index !== -1) {
+            counter++;
+            numRnd[index] = null;
+        }
+
       }   
       const result = document.getElementById('result')
       result.innerText = `Complimenti, hai indovinato ${counter} numeri!`
